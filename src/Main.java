2c6dffb0df0ef08a0eba0e8348536da3e4499e81
solar <lunar1514@naver.com>
2021-03-24 14:06:05 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,93 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.*;
+import java.nio.file.Files;
+import javax.swing.*;
+import javax.xml.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+public class Main {
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException, FileNotFoundException {
+        File dir = new File("example");
+        File []fileList = dir.listFiles();
+        int num = fileList.length;
+        String [] fileName  = new String[num];
+        String [] fileCon = new String[num];
+        BufferedReader br = null;
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+        String temp = "";
+
+        for(int i=1; i<num; i++){
+            temp = "";
+            int count = 0;
+            File file = fileList[i];
+            if(file.isFile()){
+                String name= file.getName();
+                fileName[i] = name.substring(0,name.lastIndexOf("."));
+            }
+
+
+            try{
+                FileReader reader = new FileReader(file);
+                br = new BufferedReader(reader);
+                StringBuffer sb = new StringBuffer();
+
+                while((fileCon[i] = br.readLine()) != null){
+                    if(count >= 8){
+                        temp += fileCon[i].replaceAll("<[^>]*>", " ");
+                        temp += "\n";
+                    }
+                    count++;
+
+                }
+              }catch(IOException e){
+                e.printStackTrace();
+            }finally{
+                try{
+                    if(br != null) br.close();
+                }catch(Exception e) {}
+            }
+            //System.out.println("content : "+temp);
+            fileCon[i] = temp;
+        }
+
+
+        Element docs = doc.createElement("docs");
+        doc.appendChild(docs);
+
+        for(int i=1; i<num; i++){
+            Element docId = doc.createElement("doc");
+            docs.appendChild(docId);
+
+            docId.setAttribute("id", Integer.toString(i-1));
+
+            Element title = doc.createElement("title");
+            title.appendChild(doc.createTextNode(fileName[i]));
+            docId.appendChild(title);
+
+            Element body = doc.createElement("body");
+            body.appendChild(doc.createTextNode(fileCon[i]));
+            docId.appendChild(body);
+        }
+
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new FileOutputStream(new File("example/collection.xml")));
+
+        transformer.transform(source, result);
+
+
+    }
+}
